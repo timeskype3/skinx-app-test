1feab6fd27ea99bfb79cbfa4b9e477b4c8bea586
timeskype3 <timeskype3@naver.com>
2023-01-12 15:57:22 +0700

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import LoginPage from './pages/LoginPage';
 import './App.css';
 
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <LoginPage />
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const AppWithRouter = () => (
+  <Router>
+    <App />
+  </Router>
+)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AppWithRouter />
   </React.StrictMode>
 );
 
